791d9367710a52620d5a7ccfd58336d6385f972a
Anh Le Hoang <alehoang@andrew.cmu.edu>
2021-10-28 23:00:15 -0400

    Init

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -8,4 +8,5 @@ blueman-applet &
 ibus-daemon -drxR
 kdeconnect-indicator &
 /usr/lib/notification-daemon-1.0/notification-daemon &
+# /usr/bin/ulauncher --hide-window --hide-window --hide-window --hide-window --hide-window --hide-window
 ulauncher &
