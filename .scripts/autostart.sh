a3e22fb0660d52d707ddb66bd0604588e871db50
Hoang Anh Le <alehoang@andrew.cmu.edu>
2022-08-14 13:09:06 -0400

    tunx404

--- .scripts/autostart.sh
+++ .scripts/autostart.sh
@@ -10,13 +10,12 @@ blueman-applet &
 ibus-daemon -drxR
 kdeconnect-indicator &
 /usr/lib/notification-daemon-1.0/notification-daemon &
-ulauncher &
 numlockx &
 fusuma &
 powerkit &
 xss-lock -- /usr/bin/slock &
 i8kmon &
-
+ulauncher &
 # /usr/bin/ulauncher --hide-window --hide-window --hide-window --hide-window --hide-window --hide-window
 
 sh /home/tunx404/.scripts/power_saving.sh off
