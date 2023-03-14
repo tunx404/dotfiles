9a52299f609467e3f80cab7ae1b34634a6b88c04
tunx404 <tunx404@gmail.com>
2023-03-14 15:15:38 +0700

    Add scripts for extracting themes

---
+++ .scripts/setup.sh
@@ -0,0 +1,7 @@
+tar -xzf /home/tunx404/.icons/Dracula.tar.gz -C /home/tunx404/.icons
+tar -xzf /home/tunx404/.icons/Tela-circle-nord.tar.gz -C /home/tunx404/.icons
+tar -xzf /home/tunx404/.icons/Tela-circle-nord-dark.tar.gz -C /home/tunx404/.icons
+
+tar -xzf /home/tunx404/.themes/gtk-master.tar.gz -C /home/tunx404/.themes
+tar -xzf /home/tunx404/.themes/Graphite-Dark-nord.tar.gz  -C /home/tunx404/.themes
+tar -xzf /home/tunx404/.themes/Graphite-Light-nord.tar.gz -C /home/tunx404/.themes
