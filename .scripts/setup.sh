af1c113bc528715643fa9fbff7fa396afe6e1cac
tunx404 <tunx404@gmail.com>
2023-03-17 16:05:17 +0700

    tunx404

--- .scripts/setup.sh
+++ .scripts/setup.sh
@@ -1,2 +1,4 @@
-tar -xzf /home/anhlh33/.icons/Dracula.tar.gz -C /home/anhlh33/.icons
-tar -xzf /home/anhlh33/.themes/gtk-master.tar.gz -C /home/anhlh33/.themes
+# tar -xzf /home/anhlh33/.icons/Dracula.tar.gz -C /home/anhlh33/.icons
+# tar -xzf /home/anhlh33/.themes/gtk-master.tar.gz -C /home/anhlh33/.themes
+unzip /home/anhlh33/.icons/Dracula.zip -d /home/anhlh33/.icons
+unzip /home/anhlh33/.themes/master.zip -d /home/anhlh33/.themes
