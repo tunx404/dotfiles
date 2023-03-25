98fcb315cc6746bd1e0c7bae13ae021658282855
tunx404 <tunx404@gmail.com>
2023-03-25 23:18:52 +0700

    tunx404

--- .scripts/add_dotfiles.sh
+++ .scripts/add_dotfiles.sh
@@ -15,7 +15,7 @@
 /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME add .config/pomotroid/user-preferences.json
 /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME add .config/baloofilerc
 # /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME add .config/darktable/purge_non_existing_images.sh
-/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME add .config/minidlna/minidlna.conf
+# /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME add .config/minidlna/minidlna.conf
 /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME add .config/Caprine/custom.css
 
 /usr/bin/git --git-dir=$HOME/.dotfiles --work-tree=$HOME add .ssh/config
@@ -30,12 +30,12 @@
 /usr/bin/git --git-dir=$HOME/.dotfiles --work-tree=$HOME add .icons/*.tar.gz
 /usr/bin/git --git-dir=$HOME/.dotfiles --work-tree=$HOME add .icons/*.zip
 
-/usr/bin/git --git-dir=$HOME/.dotfiles --work-tree=$HOME add .Xresources
+# /usr/bin/git --git-dir=$HOME/.dotfiles --work-tree=$HOME add .Xresources
 /usr/bin/git --git-dir=$HOME/.dotfiles --work-tree=$HOME add .bashrc
 /usr/bin/git --git-dir=$HOME/.dotfiles --work-tree=$HOME add .bash_aliases
 /usr/bin/git --git-dir=$HOME/.dotfiles --work-tree=$HOME add .gitconfig
 /usr/bin/git --git-dir=$HOME/.dotfiles --work-tree=$HOME add .gitignore
-/usr/bin/git --git-dir=$HOME/.dotfiles --work-tree=$HOME add .xprofile
+# /usr/bin/git --git-dir=$HOME/.dotfiles --work-tree=$HOME add .xprofile
 /usr/bin/git --git-dir=$HOME/.dotfiles --work-tree=$HOME add .xinitrc
 /usr/bin/git --git-dir=$HOME/.dotfiles --work-tree=$HOME add .wallpapers
 /usr/bin/git --git-dir=$HOME/.dotfiles --work-tree=$HOME add .i8kmon
