2243e342d6cefa3c23c700e1ff802634218698df
tunx404 <tunx404@gmail.com>
2023-06-08 17:49:33 +0700

    tunx404

--- .scripts/autostart.sh
+++ .scripts/autostart.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-xrandr --output eDP-1 --mode 1920x1080 --pos 0x0 --rotate normal;
-xrandr --output DP-1 --mode 1920x1080 --pos 1920x0 --rotate normal;
+# xrandr --output eDP-1 --mode 1920x1080 --pos 0x0 --rotate normal;
+# xrandr --output DP-1 --mode 1920x1080 --pos 1920x0 --rotate normal;
 # xrandr --output DP-2  --mode 1920x1080 --pos 1920x840 --rotate normal;
 # xrandr --output DP-3  --mode 1920x1080 --pos 3840x0   --rotate left
 
